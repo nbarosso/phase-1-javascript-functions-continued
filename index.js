77ebca56dcc2d0960e44f17a9040a32f0d8b2ce8
Nicholas Barosso <nbarosso@gmail.com>
2023-08-12 13:44:45 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // code your solution here
+
+function saturdayFun(activity){
+    if(typeof activity == 'string') {
+        return `This Saturday, I want to ${activity}!`
+    }
+    else {
+        return 'This Saturday, I want to roller-skate!'
+    }
+}
+
+function mondayWork(work) {
+    if(typeof work == 'string') {
+        return `This Monday, I will ${work}.`
+    }
+    else {
+        return 'This Monday, I will go to the office.'
+    }
+}
+
+function wrapAdjective(highlight) {
+    const niceCompliment = function (adjective) {
+      return `You are ${highlight}${adjective}${highlight}!`
+    }
+    return niceCompliment;
+}
+
+  // wrapAdjective('*')('a hard worker')
